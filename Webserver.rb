54c781f95ffd70deff1e3f897d7ff96b744247c2
sierrajulietromeo <steven.john.rich@gmail.com>
2017-11-05 22:11:42 +0000

    Test

---
+++ Webserver.rb
@@ -0,0 +1,23 @@
+require 'socket'
+
+#open a socket
+socket = TCPServer.open('0.0.0.0', 8080)
+
+p "Listening on :8080..."
+
+Signal.trap(:INT) do |sig|
+  p "Bye from a webserver!"
+  exit
+end
+
+loop {
+  connection = socket.accept
+  10.times { |i|
+  connection.puts i
+  sleep 1
+}
+
+connection.puts "Hello from the webserver"
+connection.close
+
+}
